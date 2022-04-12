ced563f7116f2b1f8a090e36985e41697644c68f
aluno <aluno@DSN1251230707>
2022-04-12 17:02:10 -0300

    Projeto navegação

---
+++ ProjetoNavegacaoTelas/src/main/java/br/com/senai/evelyngabrieli/application/PageBean.java
@@ -0,0 +1,13 @@
+package br.com.senai.evelyngabrieli.application;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@SuppressWarnings("serial")
+@Named
+@RequestScoped
+public class PageBean implements Serializable{
+
+}
