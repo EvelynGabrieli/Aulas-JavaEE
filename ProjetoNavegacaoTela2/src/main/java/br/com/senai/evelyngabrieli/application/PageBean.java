634ef0b918c7245150716f9ea8206f182095c259
EvelynGabrieli <101806418+EvelynGabrieli@users.noreply.github.com>
2022-06-09 14:31:57 -0300

    Add files via upload

---
+++ ProjetoNavegacaoTela2/src/main/java/br/com/senai/evelyngabrieli/application/PageBean.java
@@ -0,0 +1,42 @@
+package br.com.senai.evelyngabrieli.application;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@SuppressWarnings("serial")
+@Named
+@RequestScoped //Dura somente quando é solicitado
+
+public class PageBean implements Serializable {
+	
+	private String page;
+	
+	public enum Pages{
+		page_a, page_b; //enum 
+	}
+
+	public String getPage() {
+		return page;
+	}
+
+	public void setPage(String page) {
+		this.page = page;
+	}
+	
+	
+	public Pages goTo() //validação conforme o usuario sigitar a informação
+		
+		if("a".equals(page)) {
+			return Pages.page_a;
+			
+		}else if("b".equals(page)) {
+			return Pages.page_b;
+		}else {
+			return null;
+		}
+
+	}
+
+}
